d84452635edbb33e15970b883792c41191e7d5a7
Seynax <seyronax@gmail.com>
2024-05-29 19:54:57 +0200

    build(add-plugins): add checkstyle plugin

--- src/main/java/fr/seynax/profiler/Main.java
+++ src/main/java/fr/seynax/profiler/Main.java
@@ -2,7 +2,34 @@ package fr.seynax.profiler;
 
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] argsIn)
 	{
+		var test = new Test("coucou");
+		System.out.println(test.coucou);
+	}
+
+	public final static class Test
+	{
+		private String coucou;
+
+		public Test(String coucou)
+		{
+			this.coucou = coucou;
+		}
+
+		public String coucou()
+		{
+			return coucou;
+		}
+
+		public void coucou(String coucouIn)
+		{
+			this.coucou = coucouIn;
+		}
+
+		public boolean hasCoucou()
+		{
+			return coucou != null;
+		}
 	}
 }
