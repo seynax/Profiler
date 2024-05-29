02f55b06b5b79bb44bec837dec8b392b81b5169d
Seynax <seyronax@gmail.com>
2024-05-29 20:03:43 +0200

    build(ci): remove unused test code

--- src/main/java/fr/seynax/profiler/Main.java
+++ src/main/java/fr/seynax/profiler/Main.java
@@ -4,32 +4,5 @@ public class Main
 {
 	public static void main(String[] argsIn)
 	{
-		var test = new Test("coucou");
-		System.out.println(test.coucou);
-	}
-
-	public final static class Test
-	{
-		private String coucou;
-
-		public Test(String coucou)
-		{
-			this.coucou = coucou;
-		}
-
-		public String coucou()
-		{
-			return coucou;
-		}
-
-		public void coucou(String coucouIn)
-		{
-			this.coucou = coucouIn;
-		}
-
-		public boolean hasCoucou()
-		{
-			return coucou != null;
-		}
 	}
 }
